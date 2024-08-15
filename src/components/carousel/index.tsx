47118f64d0790954c56c83b0ac179743151fda05
Evandro Bitencourt <contato@flyingweb.com.br>
2024-08-15 14:02:58 -0400

    ajuste mobile carousel

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -25,12 +25,7 @@ export function Carousel({ object }: HomeProps) {
                 modules={[Autoplay, EffectFade, Parallax, Navigation]}
                 slidesPerView={1}
                 pagination={{ clickable: true }}
-                breakpoints={{
-                    320: { slidesPerView: 1, spaceBetween: 20 },
-                    480: { slidesPerView: 2, spaceBetween: 30 },
-                    640: { slidesPerView: 3, spaceBetween: 40 },
-                    992: { slidesPerView: 4, spaceBetween: 30 },
-                }}
+
                 autoplay={{
                     delay: 2500,
                     disableOnInteraction: false,
@@ -40,7 +35,7 @@ export function Carousel({ object }: HomeProps) {
             >
                 {object.metadata.slider?.map((banner) => (
                     <SwiperSlide key={banner.text}>
-                        <Image src={banner.image.url} alt={banner.text} width={1920} height={600} />
+                        <Image src={banner.image.url} alt={banner.text} width={1920} height={600} className='img-fluid' />
                     </SwiperSlide>
                 ))}
 

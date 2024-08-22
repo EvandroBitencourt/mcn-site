624a16aeab62e26b6fc6626b1222cde45a6ba6da
Evandro Bitencourt <contato@flyingweb.com.br>
2024-08-22 19:30:34 -0400

    tempo da requisição

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,6 +1,6 @@
 export async function getDataHome() {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/66b80fc9bebd376c873dabfb?read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 120 } })
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/66b80fc9bebd376c873dabfb?read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 60 } })
 
         if (!res.ok) {
             throw new Error("Failed to fetch data")
@@ -16,7 +16,7 @@ export async function getDataHome() {
 
 export async function getDataBlog() {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22noticias%22%7D&limit=10&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 120 } })
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22noticias%22%7D&limit=10&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 60 } })
 
         if (!res.ok) {
             throw new Error("Failed to fetch data")
@@ -44,7 +44,7 @@ export async function getItemBySlug(slug: string) {
     const url = `${baseUrl}?${queryParams.toString()}`
 
     try {
-        const res = await fetch(url, { next: { revalidate: 120 } })
+        const res = await fetch(url, { next: { revalidate: 60 } })
 
         if (!res.ok) {
             throw new Error("Failed get item by slug")
